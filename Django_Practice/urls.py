cb2e38db76c9127877e14a059f668d0a950fb5c4
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-29 00:45:17 +0600

    Views and url - More Examples

--- Django_Practice/urls.py
+++ Django_Practice/urls.py
@@ -5,7 +5,8 @@ from first_application import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index, name='index'),
-    path('home/', views.index, name='index'),
     path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    
 ]
--- first_application/views.py
+++ first_application/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    return HttpResponse('<h1>This is Homepage</h1> <a href="contact/">Contact</a> <a href="about/">About</a>')
+
+def contact(request):
+    return HttpResponse('<h1>This is Contact Page</h1> <a href="/">Home</a> <a href="/about/">About</a>')
+
+def about(request):
+    return HttpResponse('<h1>This is About Page</h1> <a href="/">Home</a> <a href="/contact/">Contact</a>')
