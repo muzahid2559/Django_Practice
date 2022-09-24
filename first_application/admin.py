e61a5475fae55add8501bcdf0f7d975638d52836
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-25 02:42:51 +0600

    Admin Site

--- first_application/admin.py
+++ first_application/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from first_application.models import Musician, Album
+
+
 
 # Register your models here.
+
+admin.site.register(Musician)
+admin.site.register(Album)
