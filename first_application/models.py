2c943c806a1d5d8a88aa7b467b3cfb91e31e7d92
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-25 00:00:43 +0600

    Models Overview

--- first_application/models.py
+++ first_application/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+CREATE TABLE Person (
+    "id" serial NOT NULL PRIMARY KEY,
+    "first_name" varchar(30) NOT NULL,
+    "last_name" varchar(30) NOT NULL
+);
+
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
