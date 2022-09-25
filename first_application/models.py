a59f2d5c97b3978aa96320d91fb3b845a13db1d9
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-25 11:56:24 +0600

    Models - more examples

--- first_application/models.py
+++ first_application/models.py
@@ -3,17 +3,28 @@ from django.db import models
 # Create your models here.
 
 class Musician(models.Model):
-	# id = models.AutoField(primary_key=True)
+    # id = models.AutoField(Primary_key=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
 
-    def str(self):
-        return self.first_name + "" + self.last_name
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Album(models.Model):
-	# id = models.AutoField(primary_key=True)
+    # id = models.AutoField(Primary_key=True)
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
-    num_stars = models.IntegerField()
+    rating = (
+    (1, "Worst"),
+    (2, "Bad"),
+    (3, "Not Bad"),
+    (4, "Good"),
+    (5, "Excellent!"),
+    )
+    num_stars = models.IntegerField(choices=rating)
+
+    def __str__(self):
+        return self.name + ", Rating: " + str(self.num_stars)
