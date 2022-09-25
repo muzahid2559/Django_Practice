6d72ccdb4b8b723d5d5ac6d154cd6d15f1d9ded4
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-26 00:45:24 +0600

    Form Submission

--- first_application/views.py
+++ first_application/views.py
@@ -18,4 +18,19 @@ def index(request):
 def form(request):
     new_form = forms.user_form()
     diction = {'test_form':new_form, 'heading_1':'This form is created using django library'}
+
+    if request.method == 'POST':
+        new_form = forms.user_form(request.POST)
+
+        if new_form.is_valid():
+            user_name = new_form.cleaned_data['user_name']
+            user_dob = new_form.cleaned_data['user_dob']
+            user_email = new_form.cleaned_data['user_email']
+
+
+            diction.update({'user_name':user_name})
+            diction.update({'user_dob':user_dob})
+            diction.update({'user_email':user_email})
+            diction.update({'form_submited' : 'Yes'})
+
     return render(request,'first_application/form.html',context=diction)
